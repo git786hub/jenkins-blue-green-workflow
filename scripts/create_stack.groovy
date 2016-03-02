2a80f47adfc30968bbb470525805b40ad05c6eb7
Vinay Selvaraj <vinas@amazon.com>
2016-03-02 15:30:13 -0500

    Started work on waitForASGInstancesToGoInService

--- scripts/create_stack.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env groovy
-
-@Grab(group='com.amazonaws', module='aws-java-sdk-cloudformation', version='1.10.57')
-
-import com.amazonaws.services.cloudformation.AmazonCloudFormationClient
-
-def cfnClient = new AmazonCloudFormationClient()
-
-print "From create_stack"
