f32114be47306835e7819a0da863b8403c665824
Vinay Selvaraj <vinas@amazon.com>
2016-03-02 13:52:20 -0500

    Updated with better logging

--- scripts/create_stack.py
+++ scripts/create_stack.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 
+from boto3.session import Session
+
 import boto3
 import sys
 import time
 
 cf = boto3.client('cloudformation')
 
-def create_stack(awsRegion, stackName, templateFile):
+def create_stack(stackName, templateFile):
   with open(templateFile, 'r') as file:
     template = file.read()
   cf.create_stack(StackName=stackName, TemplateBody=template)
 
-def wait_for_stack_create(awsRegion, stackName):
+def wait_for_stack_create(stackName):
   stack_description = cf.describe_stacks(StackName=stackName)
   status = stack_description['Stacks'][0]['StackStatus']
   
-  print "${stackName}: ${status}"
+  print "%s: %s", stackName, status
   
   if status in ['UPDATE_COMPLETE', 'CREATE_COMPLETE']:
-    print "Stack ${stackName} has been created"
+    print "Stack %s has been created", stackName
     sys.exit(0)
         
   elif status in ['UPDATE_IN_PROGRESS', 'UPDATE_ROLLBACK_IN_PROGRESS', 
@@ -27,18 +29,17 @@ def wait_for_stack_create(awsRegion, stackName):
     time.sleep(15)
        
   else:
-    print "Stack ${stackName} failed to create!"
+    print "Stack %s failed to create!", stackName
     sys.exit(1)
 
 def main(argv):
-  awsRegion    = sys.argv[1]
   stackName    = sys.argv[2]
   templateFile = sys.argv[3]
   
-  create_stack(awsRegion, stackName, templateFile)
+  create_stack(stackName, templateFile)
   
   while 1:
-    wait_for_stack_create(awsRegion, stackName)
+    wait_for_stack_create(stackName)
   
 if __name__ == "__main__":
     main(sys.argv[1:])
