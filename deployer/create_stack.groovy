8d782656e5a1181d77d99182a9d5e79f6c3c8519
Hobo Bob <vinay@selvaraj.com>
2016-03-02 11:32:33 -0500

    Added deployer

--- deployer/create_stack.groovy
+++ deployer/create_stack.groovy
@@ -1,7 +1,5 @@
 #!/usr/bin/env groovy
 
-@Grab('com.amazonaws:aws-java-sdk-cloudformation:1.10.57')
-
 import com.amazonaws.services.cloudformation.AmazonCloudFormationClient
 
 println "Testing groovy grab"
