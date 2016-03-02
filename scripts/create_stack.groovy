40cef2ab49b0c448741a327221f6693ac7b4acaa
Vinay Selvaraj <vinas@amazon.com>
2016-03-02 11:22:23 -0500

    Added create_stack

---
+++ scripts/create_stack.groovy
@@ -0,0 +1,7 @@
+#!/usr/bin/env groovy
+
+@Grab('com.amazonaws:aws-java-sdk-cloudformation:1.10.57')
+
+import com.amazonaws.services.cloudformation.AmazonCloudFormationClient
+
+println "Testing groovy grab"
